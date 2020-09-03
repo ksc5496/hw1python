913711f30fb5927f5059e035d785933535b75e12
ksc5496 <ksc5496@psu.edu>
2020-09-03 19:00:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+course1grade = input("Enter your course 1 letter grade: ")
+course1credit = input("Enter your course 1 credit: ")
+if course1grade = "A":
+  print("Grade point for course 1 is: 4.0")
+
